35e1af0f7e5c034f0ca5810da3b4789d8f141277
ameneghini <alberto.meneghini@buy4now.com>
2015-12-16 12:04:52 +0000

    refactoring

---
+++ Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RealProxyInterceptor
+{
+
+  public interface ILog
+  {
+    void Log(string msg, object arg = null);
+  }
+
+  public class ConsoleLogger : ILog
+  {
+    public void Log(string msg, object arg = null)
+    {
+      Console.ForegroundColor = ConsoleColor.Red;
+      Console.WriteLine(msg, arg);
+      Console.ResetColor();
+    }
+  }
+}
